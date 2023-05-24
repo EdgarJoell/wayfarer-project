9e59c9b1675672f1110d51ff7bea5de84c0b64da
Jaypad07 <jaypad07@gmail.com>
2023-05-24 15:06:40 -0700

    functionality for slides

--- src/app/display/picture-slider/picture-slider.component.ts
+++ src/app/display/picture-slider/picture-slider.component.ts
@@ -10,6 +10,31 @@ export class PictureSliderComponent {
   img2: string = '../assets/img24.jpg'
   img3: string = '../assets/img25.jpg'
   img4: string = '../assets/img26.jpg'
+
+
+  slideIndex: number = 1;
+  // showSlides(slideIndex);
+
+  //next/prev control
+  plusSlides(n :number): void {
+    this.showSlides(this.slideIndex += n)
+  }
+
+  showSlides(n: number) {
+    let i: number = 0;
+    let slides: any = document.querySelectorAll('.img-slider');
+    let dots: any = document.querySelectorAll('.dot');
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].style.display = "block";
+    dots[this.slideIndex-1].className += " active";
+  }
 }
 
 
