f5ffc026204e5a057748472d1cb0f566c3334b63
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-25 00:15:22 -0500

    Updated App component CSS, Picture Slider Component and CSS, and NavLinks Component

--- src/app/display/picture-slider/picture-slider.component.ts
+++ src/app/display/picture-slider/picture-slider.component.ts
@@ -6,48 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./picture-slider.component.css'],
 })
 export class PictureSliderComponent {
-  images: string[] = [
-    '../assets/img23.jpg',
-    '../assets/img24.jpg',
-    '../assets/img25.jpg',
-    '../assets/img26.jpg',
+  // interface?
+
+  images: any[] = [
+    {
+      source: '../assets/img23.jpg',
+      city: 'Madrid, Spain',
+    },
+    {
+      source: '../assets/img24.jpg',
+      city: 'Paris, France'
+    },
+    {
+      source: '../assets/img25.jpg',
+      city: 'Somewhere'
+    },
+    {
+      source: '../assets/img26.jpg',
+      city: 'Here'
+    }
   ];
-  
-  img1: string = '../assets/img23.jpg';
-  img2: string = '../assets/img24.jpg';
-  img3: string = '../assets/img25.jpg';
-  img4: string = '../assets/img26.jpg';
-
-  // slideIndex: number = 1;
-  // showSlides(slideIndex: number);
-
-  //next/prev control
-
-  // plusSlides(n :number): void {
-  //   this.showSlides(this.slideIndex += n)
-  // }
-
-  // plusSlider(n: number) {
-  //   this.showSlides(this.slideIndex += n)
-  // }
-
-  // showSlides(n: number): any {
-  //   let i: number = 0;
-  //   let slides: any = document.getElementsByClassName('.img-slider');
-  //   let dots: any = document.getElementsByClassName('.dot');
-  //   if (n > slides.length) {this.slideIndex = 1}
-  //   if (n < 1) {this.slideIndex = slides.length}
-  //   for (i = 0; i < slides.length; i++) {
-  //     slides[i].style.display = "none";
-  //   }
-  //   for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  //   }
-  //   slides[this.slideIndex-1].style.display = "block";
-  //   dots[this.slideIndex-1].className += " active";
-  // }
 }
-
-/* We have an array of objects because the object
-will have the img string url and have the city name
-*/
