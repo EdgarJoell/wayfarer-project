874cd33d5a27cb7e2b68501a9fba2fda6f13bcf2
Jaypad07 <jaypad07@gmail.com>
2023-05-25 09:39:14 -0700

    created cards module

---
+++ src/app/display/cards/cards.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardsComponent } from './cards/cards.component';
+
+@NgModule({
+  declarations: [
+    CardsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [CardsComponent]
+})
+export class CardsModule { }
--- src/app/display/display.module.ts
+++ src/app/display/display.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PictureSliderComponent } from './picture-slider/picture-slider.component';
+import { CardsModule } from './cards/cards.module';
 
 
 
@@ -9,7 +10,8 @@ import { PictureSliderComponent } from './picture-slider/picture-slider.componen
     PictureSliderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CardsModule
   ],
   exports: [PictureSliderComponent]
 })
