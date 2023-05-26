4090fecc3d8c79999f8f25f586e8d33a4f7cff6a
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-25 19:20:23 -0500

    Updated CSS for cities component

--- src/app/cities-page/cities-page.module.ts
+++ src/app/cities-page/cities-page.module.ts
@@ -4,12 +4,14 @@ import { CitiesComponent } from './cities/cities.component';
 import { CityInfoComponent } from './city-info/city-info.component';
 import { CityPageContainerComponent } from './city-page-container/city-page-container.component';
 import { HeaderModule } from '../header/header.module';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     CitiesComponent,
     CityInfoComponent,
     CityPageContainerComponent,
+    PostsComponent,
   ],
   imports: [CommonModule, HeaderModule],
   exports: [CitiesComponent, CityInfoComponent, CityPageContainerComponent],
