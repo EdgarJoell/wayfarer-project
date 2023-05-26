ef35a770fb2dfd36e04cfa26ca2f51b8aa91d8cc
Jaypad07 <jaypad07@gmail.com>
2023-05-25 20:23:10 -0700

    refactoring posts

---
+++ src/app/cities-page/city-info/posts.ts
@@ -0,0 +1,78 @@
+interface Posts {
+    id: number,
+    userName: string,
+    userImage: string,
+    postId: number,
+    postTitle: string,
+    postDesc: string
+}
+
+export const posts: Posts[] = [
+    {
+        id: 1,
+        userName: 'One',
+        userImage: '',
+        postId: 1,
+        postTitle: 'Post Title 1 for Post ID 1',
+        postDesc: 'Post Desc 1 for Post ID 1',
+    },
+    {
+        id: 2,
+        userName: 'Two',
+        userImage: '',
+        postId: 1,
+        postTitle: 'Post Title 2 for Post ID 1',
+        postDesc: 'Post Desc 2 for Post ID 1',
+    },
+
+    {
+        id: 3,
+        userName: 'Edgar Three',
+        userImage: '',
+        postId: 2,
+        postTitle: 'Post Title 3 for Post ID 2',
+        postDesc: 'Post Desc 3 for Post ID 2',
+    },
+    {
+        id: 4,
+        userName: 'Four',
+        userImage: '',
+        postId: 2,
+        postTitle: 'Post Title 4 for Post ID 2',
+        postDesc: 'Post Desc 4 for Post ID 2',
+    },
+
+    {
+        id: 5,
+        userName: 'JayPadillaDEV Five',
+        userImage: '',
+        postId: 3,
+        postTitle: 'Post Title 5 for Post ID 3',
+        postDesc: 'Post Desc 5 for Post ID 3',
+    },
+    {
+        id: 6,
+        userName: 'Six',
+        userImage: '',
+        postId: 3,
+        postTitle: 'Post Title 6 for Post ID 3',
+        postDesc: 'Post Desc 6 for Post ID 3',
+    },
+
+    {
+        id: 7,
+        userName: 'KC Seven',
+        userImage: '',
+        postId: 4,
+        postTitle: 'Post Title 7 for Post ID 4',
+        postDesc: 'Post Desc 7 for Post ID 4',
+    },
+    {
+        id: 8,
+        userName: 'Eight',
+        userImage: '',
+        postId: 4,
+        postTitle: 'Post Title 8 for Post ID 4',
+        postDesc: 'Post Desc 8 for Post ID 4',
+    },
+]
