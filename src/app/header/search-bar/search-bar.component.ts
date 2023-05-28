47c3e6bf80af6fecbe24c957669e91d925e265ad
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-27 23:32:13 -0500

    Search bar rendering results

--- src/app/header/search-bar/search-bar.component.ts
+++ src/app/header/search-bar/search-bar.component.ts
@@ -1,43 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-// import { posts } from 'src/app/cities-page/city-page-container/data-posts';
 import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
 import { SearchService } from 'src/app/services/search.service';
 import { PostsService } from 'src/app/services/posts.service';
-
+import { HttpClient } from '@angular/common/http';
+import { posts } from 'src/app/cities-page/city-page-container/data-posts';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
-export class SearchBarComponent implements OnInit{
-  id: string = ''
-  post: any;
-  posts = this.postService.getPosts();
+export class SearchBarComponent implements OnInit {
+  // id: string = ''
   searchResults: any[] = [];
   searchSubject = new Subject();
+  post: any;
 
-  constructor(private searchService: SearchService, private postService: PostsService) {}
+  searchQuery: string = '';
+  posts = this.postService.getPosts();
 
+  searchText: string = '';
+  filteredPosts: any[] = [];
 
+  constructor(
+    private searchService: SearchService,
+    private postService: PostsService,
+    private http: HttpClient
+  ) {}
 
-  findPostId(id: any) {
-  this.searchResults = [];
-
-    if(id){
-      this.searchResults = this.posts.filter(post => post.id === parseInt(id))
-    }
+  filterPosts() {
+    this.filteredPosts = this.searchText
+      ? posts.filter((post) =>
+          post.title.toLowerCase().includes(this.searchText.toLowerCase())
+        )
+      : [];
+      console.log(this.filteredPosts);
   }
+
+  selectPost(post: any) {
+    // Do something with the selected post
+    console.log(post);
+  }
+
+  // findPostId(id: any) {
+  // this.searchResults = [];
+
+  //   if(id){
+  //     this.searchResults = this.posts.filter(post => post.id === parseInt(id))
+  //   }
+  // }
+
+  // searchPosts() {
+  //   const url = `https://localhost:4200/posts?title=${this.searchQuery}`;
+  //   this.http.get<any[]>(url).subscribe((data) => {
+  //     this.posts = data;
+  //     console.log(this.posts);
+  //   });
+  // }
 
   ngOnInit(): void {
     this.searchSubject
-    .pipe(debounceTime(1000), distinctUntilChanged())
-    .subscribe((id) => {
-      this.searchService.getPostPageById(id)
-      .subscribe((res) => {
-        console.log(res);
-        this.post = res;
-      })
-    })
+      .pipe(debounceTime(1000), distinctUntilChanged())
+      .subscribe((id) => {
+        this.searchService.getPostPageById(id).subscribe((res) => {
+          console.log(res);
+          this.post = res;
+        });
+      });
   }
 }
