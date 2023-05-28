95e5888c68a467cb07989c00f5b231c1f227a799
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-28 15:29:30 -0500

    Current progress with adding comments and docstrings

--- src/app/cities-page/posts/posts.component.ts
+++ src/app/cities-page/posts/posts.component.ts
@@ -8,16 +8,28 @@ import { PostsService } from '../../services/posts.service';
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  // PostsService to retrieve all posts in the database and assigned to the private variable posts
   posts = this.postService.getPosts();
-  city: any;
-  postArr: any;
+  // Will be the result of the filtered posts from the ngOnInit() method
+  postsArr: any;
 
-  constructor(private route: ActivatedRoute, private postService: PostsService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService
+  ) {}
 
+  /**
+   * Upon initialization when a post is selected, the post's comments are loaded. The id is grabbed the the paramMap.subscribe call, and compared with the posts database to check for a matching post ID.
+   */
   ngOnInit(): void {
-      this.route.paramMap.subscribe(params => {
+    // Subscribe to changes in the URL route parameters
+    this.route.paramMap.subscribe((params) => {
+      // Retrieve the ID value from the URL route parameters and assign it to cityId
       let cityId: string = params.get('id') || '';
-      this.postArr = this.posts.filter((post) => post.postId === parseInt(cityId));
+      // Filter the posts array based on the postId matching the cityId value
+      this.postsArr = this.posts.filter(
+        (post) => post.postId === parseInt(cityId)
+      );
     });
   }
 }
--- src/app/header/search-bar/search-bar.component.ts
+++ src/app/header/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { posts } from '../../cities-page/city-page-container/data-posts';
+import { PostsService } from '../../services/posts.service';
+PostsService
 
 @Component({
   selector: 'app-search-bar',
@@ -11,12 +12,16 @@ export class SearchBarComponent {
   searchBarText: string = '';
   // Holds filtered results
   filteredPosts: any[] = [];
+  // PostsService to retrieve all posts in the database and assigned to the private variable posts
+  private posts = this.postsService.getPosts()
+
+  constructor(private postsService: PostsService) {}
 
   filterPosts() {
     // Assigns the filteredPosts array with the filtered results from the user's input made in the two-way binding through the searchBarText initializing
     this.filteredPosts = this.searchBarText
       ? // filter through the list of posts in our database and return search results by the title of the post from the searchBarText value
-        posts.filter(
+        this.posts.filter(
           (post) =>
             // compare the user's input values without worrying about what case is entered in the search box
             post.title.toLowerCase().includes(this.searchBarText.toLowerCase())
