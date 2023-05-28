b0a3b7fcf9023c8273ce7e13c7e0f7c658900dc7
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-28 13:52:18 -0500

    Search bar component complete

--- src/app/header/search-bar/search-bar.component.ts
+++ src/app/header/search-bar/search-bar.component.ts
@@ -1,68 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-// import { PostsService } from 'src/app/services/posts.service';
-
-// import { posts } from 'src/app/cities-page/city-page-container/data-posts';
-
-import { PostsService } from '../../services/posts.service';
+import { Component } from '@angular/core';
 import { posts } from '../../cities-page/city-page-container/data-posts';
-
-// import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
-// import { SearchService } from 'src/app/services/search.service';
-// import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css'],
 })
-export class SearchBarComponent implements OnInit {
-  posts = this.postService.getPosts();
+export class SearchBarComponent {
   post: any;
   searchText: string = '';
   filteredPosts: any[] = [];
-
-  constructor(
-    private postService: PostsService
-    // private searchService: SearchService,
-    // private http: HttpClient
-  ) {}
 
   filterPosts() {
     this.filteredPosts = this.searchText
       ? posts.filter((post) =>
           post.title.toLowerCase().includes(this.searchText.toLowerCase())
-        )
-      : [];
-
-    if (this.filteredPosts.length === 0) {
-      this.filteredPosts.push({ title: 'No results' });
-    }
-    console.log(this.filteredPosts);
+        ) : [];
   }
 
+  // Clears search bar text after a user clicks on a link which also removes the dropdown list
   clearSearchBar() {
-    this.searchText = ''
+    this.searchText = '';
   }
 
-  ngOnInit(): void {}
 }
-
-// ngOnInit(): void {
-// this.searchSubject
-//   .pipe(debounceTime(1000), distinctUntilChanged())
-//   .subscribe((id) => {
-//     this.searchService.getPostPageById(id).subscribe((res) => {
-//       console.log(res);
-//       this.post = res;
-//     });
-//   });
-// }
-
-// selectPost(post: any) {
-//   console.log(post);
-// }
-
-// searchSubject = new Subject();
-// searchQuery: string = '';
-// isSearchActive: boolean=false;
-// searchResults: any[] = [];
