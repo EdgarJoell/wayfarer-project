48eb1bd21b8e1a774c81cf97709fb0ac99cbd500
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-29 03:28:17 -0500

    Adding untracked files

---
+++ src/app/cities-page/weather/weather-widget/weather-widget.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../../services/weather.service';
+
+@Component({
+  selector: 'app-weather-widget',
+  templateUrl: './weather-widget.component.html',
+  styleUrls: ['./weather-widget.component.css'],
+})
+export class WeatherWidgetComponent implements OnInit {
+  weatherData!: any;
+  city!: string;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    // this.weatherData = {
+    //   main: {},
+    // };
+    // this.getWeather();
+  }
+
+  getWeather(): void {
+    this.weatherService.getWeather(this.city)
+    .subscribe((data) => {
+      this.weatherData = data;
+    });
+  }
+}
