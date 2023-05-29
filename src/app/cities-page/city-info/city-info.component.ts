c05cb5a59c97ba4eb22ba3d2dcd116545f2324f4
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-29 03:21:19 -0500

    Completed weather widget

--- src/app/cities-page/city-info/city-info.component.ts
+++ src/app/cities-page/city-info/city-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { CitiesService } from '../../services/cities.service';
+import { WeatherService } from '../../services/weather.service';
 
 /**
  * Component responsible for displaying detailed information about a specific city.
@@ -13,18 +14,13 @@ import { CitiesService } from '../../services/cities.service';
   styleUrls: ['./city-info.component.css'],
 })
 export class CityInfoComponent implements OnInit {
-  /**
-   * Array containing all the cities.
-   */
+  // Array containing all the cities.
   cities = this.citiesService.getAllCities();
+  // Current city selected based on the route parameter.
+  city: any;
+  weatherData: any;
 
   /**
-   * Current city selected based on the route parameter.
-   */
-
-  city: any;
-
- /**
    * Constructs an instance of CityInfoComponent.
    * @param route The ActivatedRoute to access the route parameter.
    * @param citiesService The CitiesService to retrieve the list of cities.
@@ -32,13 +28,25 @@ export class CityInfoComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private citiesService: CitiesService
+    private citiesService: CitiesService,
+    private weatherService: WeatherService
   ) {}
 
- /**
+  /**
    * Lifecycle hook called after the component has been initialized.
    * Retrieves the route parameter and finds the corresponding city.
    */
+
+  getWeather(): void {
+    console.log(this.city.name)
+    this.weatherService.getWeather(this.city.name.toLowerCase()).subscribe((data) => {
+      this.weatherData = data;
+    });
+  }
+
+  convertKelvinsToFahrenheit(temp: number): number {
+    return Math.round((temp - 273.15) * 9 / 5 + 32);
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
@@ -46,6 +54,7 @@ export class CityInfoComponent implements OnInit {
         let paramId: string = params.get('id') || '';
         return city.id === parseInt(paramId);
       });
+      this.getWeather()
     });
   }
 }
